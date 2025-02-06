11e85ea6337a3f6afcb148c33d9672f40c53fe56
Yoshi Askharoun <jjask7@gmail.com>
2025-02-06 15:11:22 -0600

    Add helper functions for creating new archives and flushing changes

---
+++ src/FolderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using SharpCompress.Archives;
+using SharpCompress.Common;
+
+namespace OwlCore.Storage.SharpCompress;
+
+public static class FolderExtensions
+{
+    public static async Task<ArchiveFolder> CreateArchiveAsync(this IModifiableFolder parentFolder, string name,
+        ArchiveType archiveType, CancellationToken cancellationToken = default)
+    {
+        return await ArchiveFolder.CreateArchiveAsync(parentFolder, name, archiveType, cancellationToken);
+    }
+    
+    public static async Task FlushToAsync(this IWritableArchive archive, IFile file,
+        CancellationToken cancellationToken = default)
+    {
+        await ArchiveFolder.FlushToAsync(file, archive, cancellationToken);
+    }
+}
